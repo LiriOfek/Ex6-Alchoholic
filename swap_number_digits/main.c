b6dc926ee386aca7bb10fb16cd4ede7cc000a18c
LiriOfek <46228594+LiriOfek@users.noreply.github.com>
2020-04-07 10:02:01 +0300

    add function that swap array elements

--- swap_number_digits/main.c
+++ swap_number_digits/main.c
@@ -8,6 +8,8 @@ Purpose: This file run the program that swap number
 #define INITIAL_INDEX (0)
 #define DECIMAL (10)
 #define ONE (1)
+#define INITIAL_NUMBER (0)
+#define MIDDLE_OF_ARRAY (2)
 
 unsigned int* convert_number_to_array(unsigned int *number_array,
 	int number_of_digits,
@@ -42,5 +44,46 @@ unsigned int* convert_number_to_array(unsigned int *number_array,
 		original_number /= DECIMAL;
 	}
 
+	return number_array;
+}
+
+unsigned int* swap_array_elements(unsigned int *number_array,
+	int number_of_digits)
+{
+	/********************************************************\
+	* Function name - swap_array_elements
+	*
+	* Function Purpose - reverse array by swap first element
+	*					 with last element and so on
+	*
+	* Parameters - INOUT unsigned int *number_array - the array that contain the
+	*									 digits of the number, that will be
+	*									swapped
+	*			   IN int number of digits - the number of the digits
+	*									in the given number, which is
+	*									the size of the array
+	*
+	* Return Value - swapped given array_number
+	*
+	* Side Effects - this function swap the elements in number_array
+	*
+	* Semantics - reverse array by swap first element
+	*			 with last element, second element with element
+	*			before last element and so on
+	*
+	* Author - Liri
+	\********************************************************/
+	int index = INITIAL_INDEX;
+	int current_left_element = INITIAL_NUMBER;
+
+	while (index < number_of_digits / MIDDLE_OF_ARRAY)
+	{
+		/*swap first element with last element and so on*/
+		current_left_element = number_array[index];
+		number_array[index] = number_array[number_of_digits - index - ONE];
+		number_array[number_of_digits - index - ONE] = current_left_element;
+
+		index += ONE;
+	}
 	return number_array;
 }
