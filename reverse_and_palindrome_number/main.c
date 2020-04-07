775d514ced83bc964f236f4589aa6b7202cc4b74
LiriOfek <46228594+LiriOfek@users.noreply.github.com>
2020-04-07 08:45:27 +0300

    add main function and documentation

---
+++ reverse_and_palindrome_number/main.c
@@ -0,0 +1,6 @@
+/********************************************************\
+File Name: main.c
+Author: Liri (6/4/2020)
+Purpose: This file run the program that reverse number
+and check if the number is palindrome
+\********************************************************/
