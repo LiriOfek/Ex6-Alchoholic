426db44bd9aaad4c894dd3af30e67f69bc24ef34
LiriOfek <46228594+LiriOfek@users.noreply.github.com>
2020-04-07 09:58:53 +0300

    add main file with documentation

---
+++ swap_number_digits/main.c
@@ -0,0 +1,6 @@
+/********************************************************\
+File Name: main.c
+Author: Liri (6/4/2020)
+Purpose: This file run the program that swap number
+		  by swap each corresponding elements
+\********************************************************/
